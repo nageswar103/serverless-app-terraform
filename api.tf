4f76f4f26d17847eb043da77db679b35e6e5c7be
nagesh <72961117+nageswar103@users.noreply.github.com>
2025-06-18 03:03:54 +0530

    Create api.tf

---
+++ api.tf
@@ -0,0 +1,34 @@
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.app_lambda.invoke_arn
+}
+
+
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.app_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_deployment" "api_deploy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
+resource "aws_api_gateway_stage" "api_stage" {
+  deployment_id = aws_api_gateway_deployment.api_deploy.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  stage_name    = "prod"  # You can change this to dev/uat/etc.
+}
