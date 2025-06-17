1d6dd0df22a72c4f0223378bcf6f6e75adf09313
nagesh <72961117+nageswar103@users.noreply.github.com>
2025-06-18 02:53:57 +0530

    Create iam.tf

---
+++ iam.tf
@@ -0,0 +1,23 @@
+resource "aws_iam_role" "lambda_exec" {
+  name = "nagesh-lambda-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamo" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
